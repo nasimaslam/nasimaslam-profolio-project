d1b8a3641206a8aae2a456ff1c78ac85ee92bc1a
nasimaslam <71394896+nasimaslam@users.noreply.github.com>
2020-09-16 21:19:16 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+
+
+        if($(this).scrollTop()>10){
+
+            $('header').addClass('sticky');
+        }else{
+
+            $('header').removeClass('sticky');
+        }
+    });
+});
